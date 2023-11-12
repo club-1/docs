5a77204faf3ec34da015757fb8dafc5d7d99d02c
Nicolas Peugnet <nicolas@club1.fr>
2023-11-12 17:29:34 +0100

    add matrix: to the list of allowed URL schemes

--- conf.py
+++ conf.py
@@ -90,6 +90,9 @@ myst_substitutions = {
     'docutils_version': docutils_version,
 }
 
+# Allowed URL schemes
+myst_url_schemes = ['http', 'https', 'mailto', 'matrix']
+
 # Strip everything after the last emdash in term's tooltips.
 term_tooltips_apply_function = lambda s: s.rsplit('—', 1)[0].strip()
 
