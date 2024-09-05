f4309e587bb47ecd2f290494a103ef6d16ac7b08
n-peugnet <n-peugnet@users.noreply.github.com>
2024-09-05 15:59:48 +0000

    Deploy preview for PR 312 🛫

---
+++ pr-preview/pr-312/fr/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '2024-09-05-gbf82',
+    LANGUAGE: 'fr',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: false,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: true,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
