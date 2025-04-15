401bc279980d21aff4d15e5ab6c389aebcb190a5
n-peugnet <n-peugnet@users.noreply.github.com>
2025-04-15 10:57:32 +0000

    Deploy preview for PR 330 🛫

---
+++ pr-preview/pr-330/fr/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '2025-04-15-g5029',
+    LANGUAGE: 'fr',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: false,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: true,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
